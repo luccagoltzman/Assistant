63f1ed8854c942bcf9d0f542e4d6c785f7fcbc15
luccagoltzman <liccagoltzman@gmail.com>
2024-06-20 10:19:15 -0300

    feat: :zap: Pequenos ajustes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function populateVoices() {
     voices = window.speechSynthesis.getVoices();
     console.log(voices); // Verifique as vozes disponíveis no console
     if (voices.length) {
-        speak("Iniciando CANGALHA...", 1.2);  // Fala mais rápida
+        speak("INICIANDO CANGALHA...", 1.3);  // Fala mais rápida
         wishMe();
     }
 }
@@ -25,7 +25,7 @@ function speak(text, rate = 1) {
     const text_speak = new SpeechSynthesisUtterance(text);
     setVoice(text_speak);
 
-    text_speak.rate = rate;  // Ajuste a velocidade da fala aqui
+    text_speak.rate = rate; 
     text_speak.volume = 1;
     text_speak.pitch = 1;
 
@@ -38,11 +38,11 @@ function wishMe() {
     const hour = day.getHours();
 
     if (hour >= 0 && hour < 12) {
-        speak("Bom dia moço...", 1.2);  // Fala mais rápida
+        speak("Bom dia moço...", 1.5); 
     } else if (hour >= 12 && hour < 17) {
-        speak("Boa tarde moço...", 1.2);  // Fala mais rápida
+        speak("Boa tarde moço...", 1.5);  
     } else {
-        speak("Boa noite moço...", 1.2);  // Fala mais rápida
+        speak("Boa noite moço...", 1.5);
     }
 }
 
@@ -68,59 +68,59 @@ recognition.onresult = (event) => {
 };
 
 btn.addEventListener('click', () => {
-    content.textContent = "Estou ouvindo...";
+    content.textContent = "Estou escutando...";
     recognition.start();
 });
 
 function takeCommand(message) {
-    if (message.includes('hey') || message.includes('hello') || message.includes('oi') || message.includes('oi, cangalha') || message.includes('olá, cangalha') || message.includes('Hello, cangalha')) {
-        speak("Olá Senhor, como posso lhe ajudar?", 1.2);  // Fala mais rápida
+    if (message.includes('hey') || message.includes('hello') || message.includes('oi') || message.includes('oi, cangalha') || message.includes('olá, cangalha') || message.includes('olá') || message.includes('Hello, cangalha')) {
+        speak("Olá Senhor, como posso lhe ajudar?", 1.5);  
     }
     else if (message.includes('tudo bem') || message.includes('como você está') || message.includes('beleza') || message.includes('tudo beleza') || message.includes('como você está hoje') || message.includes('como vai')) {
-        speak("Estou muito bem! E você?", 1.2);  // Fala mais rápida
+        speak("Estou muito bem! E você?", 1.5);  
     }
     else if (message.includes('está me escutando') || message.includes('não conseguiu me escutar')) {
-        speak("Análise... Desculpa a demora, senhor! É o TDH", 1.2);  // Fala mais rápida
+        speak("Análise... Estou sim! Mas tenha pacieência com o meu TDH", 1.5); 
     }
     else if (message.includes('tchau') || message.includes('tchau cangalha') || message.includes('até logo') || message.includes('adeus')) {
-        speak("Tem certeza? Sentirei sua falta...", 1.2);  // Fala mais rápida
+        speak("Tem certeza? Sentirei sua falta...", 1.5);  
     }
     else if (message.includes("Abrir google") || message.includes('Google')) {
         window.open("https://google.com", "_blank");
-        speak("Abrindo Google", 1.2);  // Fala mais rápida
+        speak("Abrindo Google", 1.5);  
     } else if (message.includes("open youtube") || message.includes('youtube')) {
         window.open("https://youtube.com", "_blank");
-        speak("Abrindo Youtube...", 1.2);  // Fala mais rápida
+        speak("Abrindo Youtube...", 1.5); 
     } else if (message.includes("abrir linkedin")) {
         window.open("https://www.linkedin.com", "_blank");
-        speak("Abrindo Linkedin...", 1.2);  // Fala mais rápida
+        speak("Abrindo Linkedin...", 1.5); 
     } else if (message.includes('what is') || message.includes('who is') || message.includes('what are')) {
         window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
         const finalText = "Isto é o que encontrei na internet sobre " + message;
-        speak(finalText, 1.2);  // Fala mais rápida
+        speak(finalText, 1.5);  
     } else if (message.includes('wikipedia')) {
         window.open(`https://en.wikipedia.org/wiki/${message.replace("wikipedia", "").trim()}`, "_blank");
         const finalText = "Isto é o que encontrei na Wikipedia sobre " + message;
-        speak(finalText, 1.2);  // Fala mais rápida
+        speak(finalText, 1.5); 
     } else if (message.includes('Time') || message.includes('Que horas são?') || message.includes('Me diga as horas') || message.includes('horas')) {
         const time = new Date().toLocaleString(undefined, { hour: "numeric", minute: "numeric" });
         const finalText = "São exatamente " + time;
-        speak(finalText, 1.2);  // Fala mais rápida
+        speak(finalText, 1.5); 
     } else if (message.includes('date') || message.includes('data') || message.includes('que dia é hoje')) {
         const date = new Date().toLocaleString(undefined, { month: "short", day: "numeric" });
         const finalText = "A data de hoje é " + date;
-        speak(finalText, 1.2);  // Fala mais rápida
+        speak(finalText, 1.5); 
     } else if (message.includes('calculator') || message.includes('calculadora')) {
         window.open('Calculator:///');
         const finalText = "Abrindo Calculadora";
-        speak(finalText, 1.2);  // Fala mais rápida
+        speak(finalText, 1.5);
     } else if (message.includes('Bloco de Notas') || message.includes('Notas')) {
         window.open('Bloco de Notas:///');
         const finalText = "Abrindo Bloco de notas";
-        speak(finalText, 1.2);  // Fala mais rápida
+        speak(finalText, 1.5);
     } else {
         window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
         const finalText = "Encontrei algumas informações para " + message + " no Google";
-        speak(finalText, 1.2);  // Fala mais rápida
+        speak(finalText, 1.5); 
     }
 }
